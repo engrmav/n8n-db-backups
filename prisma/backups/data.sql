cdbc75d744f56044eb7f9b754cf14f1dce7565cc
engrmav <engrmav@users.noreply.github.com>
2025-11-06 01:01:12 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict eAcnxEuEXbKnJnTguZdusK3JZsegarUv2gCnWNh7DedD6cpORehdCfUlup9szd9
+-- \restrict 1iicisTMGp1oGIvv21Z2jNKbQy4xM7jYJAh3KhS3qNk9wRgTiKxEdynMP0QEgpu
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -73,7 +73,7 @@ COPY "auth"."oauth_clients" ("id", "client_secret_hash", "registration_type", "r
 -- Data for Name: sessions; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
 --
 
-COPY "auth"."sessions" ("id", "user_id", "created_at", "updated_at", "factor_id", "aal", "not_after", "refreshed_at", "user_agent", "ip", "tag", "oauth_client_id") FROM stdin;
+COPY "auth"."sessions" ("id", "user_id", "created_at", "updated_at", "factor_id", "aal", "not_after", "refreshed_at", "user_agent", "ip", "tag", "oauth_client_id", "refresh_token_hmac_key", "refresh_token_counter") FROM stdin;
 \.
 
 
@@ -89,7 +89,7 @@ COPY "auth"."mfa_amr_claims" ("session_id", "created_at", "updated_at", "authent
 -- Data for Name: mfa_factors; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
 --
 
-COPY "auth"."mfa_factors" ("id", "user_id", "friendly_name", "factor_type", "status", "created_at", "updated_at", "secret", "phone", "last_challenged_at", "web_authn_credential", "web_authn_aaguid") FROM stdin;
+COPY "auth"."mfa_factors" ("id", "user_id", "friendly_name", "factor_type", "status", "created_at", "updated_at", "secret", "phone", "last_challenged_at", "web_authn_credential", "web_authn_aaguid", "last_webauthn_challenge_data") FROM stdin;
 \.
 
 
@@ -1202,6 +1202,6 @@ SELECT pg_catalog.setval('"public"."migrations_id_seq"', 101, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict eAcnxEuEXbKnJnTguZdusK3JZsegarUv2gCnWNh7DedD6cpORehdCfUlup9szd9
+-- \unrestrict 1iicisTMGp1oGIvv21Z2jNKbQy4xM7jYJAh3KhS3qNk9wRgTiKxEdynMP0QEgpu
 
 RESET ALL;
