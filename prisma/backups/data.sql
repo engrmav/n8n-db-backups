11d64e6ddbbdbe02ca671c8a7419f9c2f2728d00
engrmav <engrmav@users.noreply.github.com>
2026-02-26 01:19:04 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict KMsct5AKaQk1Fx4uodGpOTBYPLShvhxedVij4i6cOhP4k0N3t3i1AsgQgLbXKjR
+-- \restrict zjXbHg1n66E0EC5UPHPKZ14zcO7NrQqSz6iKyBMKeGIpx4NhrzjARyyCd33BlAX
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -26,6 +26,14 @@ SET row_security = off;
 --
 
 COPY "auth"."audit_log_entries" ("instance_id", "id", "payload", "created_at", "ip_address") FROM stdin;
+\.
+
+
+--
+-- Data for Name: custom_oauth_providers; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+COPY "auth"."custom_oauth_providers" ("id", "provider_type", "identifier", "name", "client_id", "client_secret", "acceptable_client_ids", "scopes", "pkce_enabled", "attribute_mapping", "authorization_params", "enabled", "email_optional", "issuer", "discovery_url", "skip_nonce_check", "cached_discovery", "discovery_cached_at", "authorization_url", "token_url", "userinfo_url", "jwks_uri", "created_at", "updated_at") FROM stdin;
 \.
 
 
@@ -1210,6 +1218,6 @@ SELECT pg_catalog.setval('"public"."migrations_id_seq"', 101, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict KMsct5AKaQk1Fx4uodGpOTBYPLShvhxedVij4i6cOhP4k0N3t3i1AsgQgLbXKjR
+-- \unrestrict zjXbHg1n66E0EC5UPHPKZ14zcO7NrQqSz6iKyBMKeGIpx4NhrzjARyyCd33BlAX
 
 RESET ALL;
