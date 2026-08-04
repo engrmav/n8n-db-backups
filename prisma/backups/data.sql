5a27c6ec78048cc1011013662725c30867f6a419
engrmav <engrmav@users.noreply.github.com>
2026-08-04 01:44:25 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict 8r56gz4lrhzvVL0UeTHQBLQyszebdaeeMSTxaHOUfVLuuxNcYyaetgDCyHQ3Cb2
+-- \restrict AJZ5wA0rymH9ay3PEpfzipIttWPuhvVnB6JVJUZ41tx9l0bhne48PYs3km53WSe
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -1234,6 +1234,6 @@ SELECT pg_catalog.setval('"public"."migrations_id_seq"', 101, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict 8r56gz4lrhzvVL0UeTHQBLQyszebdaeeMSTxaHOUfVLuuxNcYyaetgDCyHQ3Cb2
+-- \unrestrict AJZ5wA0rymH9ay3PEpfzipIttWPuhvVnB6JVJUZ41tx9l0bhne48PYs3km53WSe
 
 RESET ALL;
